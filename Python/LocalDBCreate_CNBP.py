4d295a1cd5670c740f32f49f50a63c8e7bed45ae
Yang Ding <it@cnbp.ca>
2018-07-13 09:31:28 -0400

    Comlpeted LocalDBCreate_CNBP
    Also added rudimentary test. Need to compelte the unit test more.

--- Python/LocalDBCreate_CNBP.py
+++ Python/LocalDBCreate_CNBP.py
@@ -2,74 +2,70 @@
 #Creation: 2018-07-11T160228EST
 #Author: Yang Ding
 
-
+import sys
+import os
 import sqlite3
 import logging
 from pathlib import Path
+import argparse
+import getpass
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-logger = logging.getLogger('upload_bom')
+def LocalDBCreate_CNBP():
 
-sqliteFile = '..\LocalDB\LocalDB_CNBPs.sqlite'    # name of the sqlite database file
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logger = logging.getLogger('upload_bom')
 
-# name of the table to be created
-tableName = 'id_table'
+    # name of the sqlite database file
+    sqliteFile = '..\LocalDB\LocalDB_CNBPs.sqlite'
 
+    #If SQL already exist, quit script.
+    SQLPath = Path(sqliteFile)
+    if SQLPath.is_file():
+        #logger.info('SQLite database file already exist. Not gonna mess with it!')
+        #quit()
 
-#Create the PRIMARY KEY column.
-new_field = 'DBKEY'
-field_type = 'INTEGER'  # column data type
-
-#Create the MRN column.
-new_field1 = 'MNR' # name of the column
-field_type1 = 'CHAR(10)'  # column data type
-
-#Create the CNBPID column
-new_field2 = 'CNBPID' # name of the column
-field_type2 = 'CHAR(50)'  # column data type
-
-#Create the CNBPID column
-new_field3 = 'CNNID' # name of the column
-field_type3 = 'CHAR(12)'  # column data type
-
-new_field4 = 'CNFUNID' # name of the column
-field_type4 = 'CHAR(12)'  # column data type
-
-new_field5 = 'Hash_MotherMRN' # name of the column
-field_type5 = 'CHAR(20)'  # column data type
+        '''Delete current database! During testing only'''
+        os.remove(sqliteFile)
+        logger.info('DEBUG: database file already exist. Deleted it!')
 
 
-#If SQL already exist, quit script.
-SQLPath = Path(sqliteFile)
-if SQLPath.is_file():
-    logger.info('SQLite database file already exist. Not gonna mess with it!')
-    quit()
+    # name of the table to be created
+    tableName = 'id_table'
 
 
-# Create on Connecting to the database file
-ConnectedDatabase = sqlite3.connect(sqliteFile)
+    #Create the PRIMARY KEY column.
+    new_field = 'DBKEY'
+    field_type = 'INTEGER'  # column data type
 
+    #Create the variable array that store the columns information to be used later in loop for column creation
+    newColumns = ['MRN','CNBPID','CNNID','CNFUNID','Hash1','Hash2','Hash3']
+    newColumnsTypes = ['INTEGER', 'TEXT', 'INTEGER', 'INTEGER', 'TEXT', 'TEXT', 'TEXT']
+    newColumnSpec = zip(newColumns, newColumnsTypes)
+    newColumnSpecList = list(newColumnSpec)
 
+    # Create on Connecting to the database file
+    ConnectedDatabase = sqlite3.connect(sqliteFile)
 
-c = ConnectedDatabase.cursor()
+    c = ConnectedDatabase.cursor()
 
-# Creating a new SQLite table with 1 column
+    logger.info('Creating PRIMARY KEY DBKEY column in database.')
 
-CommandString = 'CREATE TABLE' tablename ()
+    # Creating a new SQLite table with DBKey column (inspired by: https://sebastianraschka.com/Articles/2014_sqlite_in_python_tutorial.html)
+    c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'.format(tn=tableName, nf=new_field, ft=field_type))
 
-c.execute('CREATE TABLE {tn} ({nf} {ft})'.format(tn=tableName, nf=new_field, ft=field_type))
+    logger.info('PRIMARY KEY DBKEY column successfully created in database.')
 
-CREATE TABLE `Sfasdf` (
-	`Test`	INTEGER,
-	`Field2`	TEXT,
-	PRIMARY KEY(`Test`)
-);
+    logger.info('Creating secondary columns in database.')
 
-# Creating a second table with 1 column and set it as PRIMARY KEY
-# note that PRIMARY KEY column must consist of unique values!
+    # Adding accessory columns via a loop
+    for column in newColumnSpecList:
+        c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}".format(tn=tableName, cn=column[0], ct=column[1]))
+    logger.info('Secondary columns created in database.')
 
-#c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'.format(tn=table_name2, nf=new_field, ft=field_type))
+    # Committing changes and closing the connection to the database file
+    ConnectedDatabase.commit()
+    ConnectedDatabase.close()
 
-# Committing changes and closing the connection to the database file
-ConnectedDatabase.commit()
-ConnectedDatabase.close()
+#Only executed when running directly.
+if __name__ == '__main__':
+    LocalDBCreate_CNBP()
