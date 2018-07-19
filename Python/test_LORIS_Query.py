1d28ce33befe903c29c5985b300b762ddeaee4c4
Yang Ding <it@cnbp.ca>
2018-07-19 12:45:30 -0400

    Updated login method to use 0.0.2 and PyCurl
    Requests library failing. Switching to someothing worse.

--- Python/test_LORIS_Query.py
+++ Python/test_LORIS_Query.py
@@ -8,21 +8,26 @@ import sys
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 def test_LORIS_login():
-    success, token = login()
-    assert success
+    logger = logging.getLogger('UT_LORIS_login')
+    response_success, token = login()
+    assert response_success
+    assert len(token) == 256 #token should always be 256 char long
 
-def test_LORIS_create():
-    success, json = getCNBP("projects")
-    assert success
-    success, json = getCNBP("candidates")
-    assert success
+def test_LORIS_get():
+    logger = logging.getLogger('UT_LORIS_get')
+    response_success, json = getCNBP("projects")
+    assert response_success
+    response_success, json = getCNBP("candidates")
+    assert response_success
 
 def test_checkPSCIDExist():
+    logger = logging.getLogger('UT_LORIS_PSCID_check')
     response_success, exist = checkPSCIDExist("CNBP0010001")
     assert response_success
     assert exist
 
 def test_checkDCCIDExist():
+    logger = logging.getLogger('UT_LORIS_DCCID_check')
     response_success, exist = checkDCCIDExist('272264')
     assert response_success
     assert exist
