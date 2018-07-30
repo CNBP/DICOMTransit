d531d5c2e974f36e5061921fcc62657a006db89f
Yang Ding <it@cnbp.ca>
2018-07-30 15:18:21 -0400

    Refactored test locations, added DICOM decompression
    Moved test locations to individual test folder. Local unit tests past.  Travis updated to get DCMTK.

--- Python/Coverage/coverage_setup.py
+++ Python/Coverage/coverage_setup.py
@@ -14,30 +14,30 @@ sys.path.append(script_root)
 print(sys.path)
 print("OKAY")
 
-from tests.test_Intermediate_Server import test_updateLocalTimepoint
+from Integration.test_Integration import test_updateLocalTimepoint
 test_updateLocalTimepoint()
 
-from tests.test_LocalDBCreate import test_LocalDBCreate, test_LocalDBCreate_CNBP
+from LocalDB.test_LocalDBCreate import test_LocalDBCreate, test_LocalDBCreate_CNBP
 test_LocalDBCreate()
 test_LocalDBCreate_CNBP()
 
-from tests.test_LORIS_Query import test_checkPSCIDExist, test_checkDCCIDExist, test_LORIS_get, test_LORIS_login
+from LORIS.test_LORIS_Query import test_checkPSCIDExist, test_checkDCCIDExist, test_LORIS_get, test_LORIS_login
 test_checkPSCIDExist()
 test_checkDCCIDExist()
 test_LORIS_get()
 test_LORIS_login()
 
-from tests.test_LocalDBQuery import test_CheckSubjectExist, test_CreateSubject, test_CreateSubjectCheckExist, test_SubjectUpdate
+from LocalDB.test_LocalDBQuery import test_CheckSubjectExist, test_CreateSubject, test_CreateSubjectCheckExist, test_SubjectUpdate
 test_CheckSubjectExist()
 test_CreateSubject()
 test_CreateSubjectCheckExist()
 test_SubjectUpdate()
 
-from tests.test_file_ops import *
+from oshelper.test_file_ops import *
 test_recursive_load()
 test_copy_files_to_flat_folder()
 
-from tests.test_DICOM import *
+from DICOM.test_DICOM import *
 
 test_DICOM_validator()
 test_DICOM_RequireDecompression()
@@ -46,8 +46,8 @@ test_DICOM_retrieveMRN()
 test_DICOM_update()
 test_DICOM_computerScanAge()
 
-from tests.test_LORIS_timepoint import *
+from LORIS.test_LORIS_timepoint import *
 test_visit_number_extraction()
 
-from tests.test_LORIS_helper import *
+from LORIS.test_LORIS_helper import *
 test_number_extraction()
