4777c2ca86e639cfee042db8e6bd90346d3daea3
Yang Ding <it@cnbp.ca>
2018-09-12 15:51:21 -0400

    Updated method to proxy to LORIS installation base to user directtory.

--- Python/LORIS/test_helper.py
+++ Python/LORIS/test_helper.py
@@ -1,5 +1,7 @@
-from LORIS.helper import LORIS_helper
+import os
 import unittest
+from LORIS.helper import LORIS_helper
+from dotenv import load_dotenv
 
 class UT_LORISHelper(unittest.TestCase):
 
@@ -18,6 +20,25 @@ class UT_LORISHelper(unittest.TestCase):
 
         for tupleItem in DualList:
             assert str(tupleItem[0]) == LORIS_helper.number_extraction(tupleItem[1])[0]
+
+
+    @staticmethod
+    def test_ProxyUpload():
+        assert load_dotenv()
+
+        ProxyIP = os.getenv("ProxyIP")
+        ProxyUsername = os.getenv("ProxyUsername")
+        ProxyPassword = os.getenv("ProxyPassword")
+        LORISHostPassword = os.getenv("LORISHostPassword")
+        LORISHostUsername = os.getenv("LORISHostUsername")
+        LORISHostIP = os.getenv("LORISHostIP")
+        Client = LORIS_helper.getProxySSHClient(ProxyIP, ProxyUsername, ProxyPassword,
+                                                LORISHostIP, LORISHostUsername, LORISHostPassword)
+        LORIS_helper.uploadThroughClient(Client, "test_helper.py", "test_helper.py")
+        sftp = Client.open_sftp()
+        sftp.remove("test_helper.py")
+        sftp.close()
+        Client.close()
 
 if __name__ == '__main__':
     UT_LORISHelper.test_number_extraction()
