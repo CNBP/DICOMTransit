8be4c05864a34bba274a4e2748e24ae2b5715833
Yang Ding <dyt811@gmail.com>
2018-11-30 16:28:01 -0500

    Added basic credential to the entire orthanc workflow
    Basic authentication is enabled for both unit tests on local development as well as prod

--- Python/Integration/fsm_precursor.py
+++ Python/Integration/fsm_precursor.py
@@ -3,69 +3,77 @@ import orthanc.API
 import LORIS.API
 import LocalDB.API
 from DICOM.DICOMPackage import DICOMPackage
+
 import os
 
 if __name__ == "__main__":
     """Quick and dirty script that calls various high level APIs to insert a subject from ORTHANC to LORIS"""
     # Eventually, a finite state machine will be built out of this
+    url, user, password = orthanc.API.get_local_orthanc_credentials()
 
     # Get list of subjects.
-    list_subjects = orthanc.API.get_list_of_subjects()
+    list_subjects = orthanc.API.get_list_of_subjects(url, user, password)
 
-    # Get first subject. #todo: this needs ot handle multiple subjects
-    dicom_folder = orthanc.API.get_subject_zip(list_subjects[0]) # if index is 0, no subject in LORIS.
+    assert len(list_subjects) > 0
 
-    # Convert it to a DICOMPackage, it checks for a bunch of validity, update a bunch of its meta information regarding the entire archive
-    DICOM_package = DICOMPackage(dicom_folder)
+    # Get subject, loop through them all.
+    for subject in list_subjects:
 
-    # Check Protocol.
-    MRN_exist = LocalDB.API.check_MRN(DICOM_package.MRN)
+        subject_url = url + subject
 
-    if MRN_exist is False:  # most common scenario first
-        # Dynamicly generate the new CNBPID based ont he protocol.
+        dicom_folder = orthanc.API.get_subject_zip(subject_url, user, password)
 
-        # todo: attempt to generate the CNBPID/PSCID based on study protocol.
-        DICOM_package.CNBPID = LocalDB.API.propose_CNBPID(DICOM_package.studies[0])
+        # Convert it to a DICOMPackage, it checks for a bunch of validity, update a bunch of its meta information regarding the entire archive
+        DICOM_package = DICOMPackage(dicom_folder)
 
-        # create new PSCID and get DCCID
-        success, DCCID = LORIS.API.create_new(DICOM_package.CNBPID, DICOM_package.birthday, DICOM_package.sex)
+        # Check Protocol.
+        MRN_exist = LocalDB.API.check_MRN(DICOM_package.MRN)
 
-        # Local Variable for anonymization.
-        DICOM_package.DCCID = DCCID
-        DICOM_package.visit = "V1" # auto generated.
+        if MRN_exist is False:  # most common scenario first
+            # Dynamicly generate the new CNBPID based ont he protocol.
 
-        # Update local database storage.
-        LocalDB.API.set_CNBP(DICOM_package.MRN, DICOM_package.CNBPID)
-        LocalDB.API.set_DCCID(DICOM_package.MRN, DCCID)
-        LocalDB.API.set_timepoint(DICOM_package.MRN, "V1")
+            # todo: attempt to generate the CNBPID/PSCID based on study protocol.
+            DICOM_package.CNBPID = LocalDB.API.propose_CNBPID(DICOM_package.studies[0])
 
-    elif MRN_exist is True:
+            # create new PSCID and get DCCID
+            success, DCCID = LORIS.API.create_new(DICOM_package.CNBPID, DICOM_package.birthday, DICOM_package.sex)
 
-        # todo: Check scan dates to see if they have already been inserted.
+            # Local Variable for anonymization.
+            DICOM_package.DCCID = DCCID
+            DICOM_package.visit = "V1" # auto generated.
 
-        # Use MRN to retrieve CNBPID, update the dicom-package
-        DICOM_package.CNBPID = LocalDB.API.get_CNBP(DICOM_package.MRN)
+            # Update local database storage.
+            LocalDB.API.set_CNBP(DICOM_package.MRN, DICOM_package.CNBPID)
+            LocalDB.API.set_DCCID(DICOM_package.MRN, DCCID)
+            LocalDB.API.set_timepoint(DICOM_package.MRN, "V1")
 
-        # Use MRN to retrieve DCCID, update the dicom-package
-        DICOM_package.DCCID = LocalDB.API.get_DCCID(DICOM_package.MRN)
+        elif MRN_exist is True:
 
-        # Use MRN to retrieve VISIT , update the dicom-package
-        DICOM_package.visit = LocalDB.API.get_visit(DICOM_package.MRN)
+            # todo: Check scan dates to see if they have already been inserted.
 
-        # Auto increment the VISIT count.
+            # Use MRN to retrieve CNBPID, update the dicom-package
+            DICOM_package.CNBPID = LocalDB.API.get_CNBP(DICOM_package.MRN)
 
-        # Write to database and also online.
-    else:
-        raise ValueError
+            # Use MRN to retrieve DCCID, update the dicom-package
+            DICOM_package.DCCID = LocalDB.API.get_DCCID(DICOM_package.MRN)
 
-    # Generate new string with everything.
-    DICOM_package.zipname = DICOM_package.CNBPID + "_" + str(DICOM_package.DCCID) + "_" + DICOM_package.visit
+            # Use MRN to retrieve VISIT , update the dicom-package
+            DICOM_package.visit = LocalDB.API.get_visit(DICOM_package.MRN)
 
-    # Anonymize to Zip
-    DICOM.API.anonymize_to_zip(DICOM_package.dicom_folder, DICOM_package.zipname)
+            # Auto increment the VISIT count.
 
-    # Upload
-    LORIS.API.upload(os.path.join(r"C:\GitHub\DICOMTransit\Python\data_archives", DICOM_package.zipname + ".zip"))
+            # Write to database and also online.
+        else:
+            raise ValueError
 
-    # Trigger insertion.
-    LORIS.API.trigger_insertion(DICOM_package.zipname)
+        # Generate new string with everything.
+        DICOM_package.zipname = DICOM_package.CNBPID + "_" + str(DICOM_package.DCCID) + "_" + DICOM_package.visit
+
+        # Anonymize to Zip
+        DICOM.API.anonymize_to_zip(DICOM_package.dicom_folder, DICOM_package.zipname)
+
+        # Upload
+        LORIS.API.upload(os.path.join(r"C:\GitHub\DICOMTransit\Python\data_archives", DICOM_package.zipname + ".zip"))
+
+        # Trigger insertion.
+        LORIS.API.trigger_insertion(DICOM_package.zipname)
--- Python/orthanc/API.py
+++ Python/orthanc/API.py
@@ -1,31 +1,46 @@
 import os
-
-
 import tempfile
 from orthanc.query import orthanc_query
 from LORIS.helper import LORIS_helper
+from PythonUtils.file import unique_name
+from PythonUtils.env import load_dotenv_var
 
-def get_list_of_subjects():
+
+def get_local_orthanc_credentials():
+    url = load_dotenv_var("TestOrthancIP")
+    user = load_dotenv_var("TestOrthacUser")
+    password = load_dotenv_var("TestOrthancPassword")
+    return url, user, password
+
+def get_prod_orthanc_credentials():
+    url = load_dotenv_var("ProdOrthancIP")
+    user = load_dotenv_var("ProdOrthacUser")
+    password = load_dotenv_var("ProdOrthancPassword")
+    return url, user, password
+
+
+def get_list_of_subjects(orthanc_URL, orthanc_user, orthanc_password):
     """
     Get a list of subjects from a .env predefined orthanc server.
     :return: the lsit of all subjects in the orthanc server
     """
-    reseponse_code, list_subjects = orthanc_query.getOrthanc("patients/")
+    endpoint = orthanc_URL + "patients/"
+    reseponse_code, list_subjects = orthanc_query.getOrthanc(endpoint, orthanc_user, orthanc_password)
     assert (LORIS_helper.is_response_success(reseponse_code, 200))
     return list_subjects
 
-def get_subject_zip(subject_UUID):
+def get_subject_zip(orthanc_URL_with_UUID, orthaner_user, orthanc_password):
     """
     Obtain the actual zip files of the subject based on the UUID given and unzip them to a temporary folder, and return it.
-    :param subject_UUID:
+    :param orthanc_URL_with_UUID:
     :return: the temporary folder object which contain the reference to the folder in the .name attribute
     """
-    status, zip_file = orthanc_query.getPatientZipOrthanc(subject_UUID)
+    status, zip_file = orthanc_query.getPatientZipOrthanc(orthanc_URL_with_UUID, orthaner_user, orthanc_password)
     assert (LORIS_helper.is_response_success(status, 200))
     assert (os.path.exists(zip_file))
 
     # Create the temporary directory
-    folder = tempfile.TemporaryDirectory(prefix=subject_UUID)
+    folder = tempfile.TemporaryDirectory(prefix=unique_name())
 
     # Unzip into the temporary directory.
     orthanc_query.flatUnZip(zip_file, folder.name)
