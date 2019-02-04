deed6c1e827a3b7b17ad18e7ab0f99990eca6e46
Martin Lamarche <martin.lamarche.urca@gmail.com>
2019-02-04 15:19:23 -0500

    Reviewed cnn_and_cnfun_to_redcap.py

--- Python/redcap/cnn_and_cnfun_to_redcap.py
+++ Python/redcap/cnn_and_cnfun_to_redcap.py
@@ -7,7 +7,7 @@ from LocalDB.API import load_hospital_record_numbers
 from redcap.prepare_patient import prepare_patient_tables
 from redcap.prepare_reference import prepare_reference_tables
 from redcap.initialization import initialize_import_configuration
-from redcap.transaction import RedcapTransaction # the class that contain all the configurations and buffer fo the records
+from redcap.transaction import RedcapTransaction
 from redcap.query import load_metadata, send_data
 from redcap.constants import environment
 import logging
@@ -19,6 +19,7 @@ import logging
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
+
 def update_redcap_data():
     """
     This method is the main method of this script. It calls all methods necessary to transfer CNN and CNFUN data
@@ -29,63 +30,62 @@ def update_redcap_data():
     window.config(cursor="wait")
 
     # Insert blank line.
-    label = Label(window, text='')
-    label.pack()
+    info_label = Label(window, text='')
+    info_label.pack()
 
-    # Initilize the RedcapTransaction class object to be past and returned each step of the way.
+    # Initialize the RedcapTransaction class object to be past and returned each step of the way.
     transaction_stage0 = RedcapTransaction()
 
-
     # Load data import configuration matrix.
-    label = Label(window, text='Loading Data Import Configuration...')
-    label.pack()
+    info_label = Label(window, text='Loading Data Import Configuration...')
+    info_label.pack()
     transaction_stage1_initialized = initialize_import_configuration(transaction_stage0)
-    label = Label(window, text='Done.')
-    label.pack()
+    info_label = Label(window, text='Done.')
+    info_label.pack()
 
     # Get all information about REDCap table names and fields.
-    label = Label(window, text='Loading REDCap Metadata...')
-    label.pack()
+    info_label = Label(window, text='Loading REDCap Metadata...')
+    info_label.pack()
     transaction_stage2_meta_added = load_metadata(transaction_stage1_initialized)
-    label = Label(window, text='Done.')
-    label.pack()
+    info_label = Label(window, text='Done.')
+    info_label.pack()
 
     # Get all hospital record numbers.
-    label = Label(window, text='Loading Hospital Record Numbers...')
-    label.pack()
-    # Change this flag in env or here to force local of DB loading.
+    info_label = Label(window, text='Loading Hospital Record Numbers...')
+    info_label.pack()
+    # Change this flag in environment module or here to force local of DB loading.
     transaction_stage2_meta_added.hospital_record_numbers = load_hospital_record_numbers(environment.USE_LOCAL_HOSPITAL_RECORD_NUMBERS_LIST)
-    label = Label(window, text='Done.')
-    label.pack()
+    info_label = Label(window, text='Done.')
+    info_label.pack()
 
-    # Update Reference Tables.
-    label = Label(window, text='Preparing Reference Data Transfer...')
-    label.pack()
+    # Prepare Reference Data.
+    info_label = Label(window, text='Preparing Reference Data Transfer...')
+    info_label.pack()
     transaction_stage3_references_added = prepare_reference_tables(transaction_stage2_meta_added)
-    label = Label(window, text='Done.')
-    label.pack()
+    info_label = Label(window, text='Done.')
+    info_label.pack()
 
-    # Update Patient Tables.
-    label = Label(window, text='Preparing Patient Data Transfer...')
-    label.pack()
+    # Prepare Patient Data.
+    info_label = Label(window, text='Preparing Patient Data Transfer...')
+    info_label.pack()
     transaction_stage4_patients_added = prepare_patient_tables(transaction_stage3_references_added)
-    label = Label(window, text='Done.')
-    label.pack()
+    info_label = Label(window, text='Done.')
+    info_label.pack()
 
     # Send data to REDCap.
-    label = Label(window, text='Sending ALL data to REDCap...')
-    label.pack()
+    info_label = Label(window, text='Sending ALL data to REDCap...')
+    info_label.pack()
     send_data(transaction_stage4_patients_added)
-    label = Label(window, text='Done.')
-    label.pack()
+    info_label = Label(window, text='Done.')
+    info_label.pack()
 
     # Insert blank line.
-    label = Label(window, text='')
-    label.pack()
+    info_label = Label(window, text='')
+    info_label.pack()
 
     # Indicate that the script is completed.
-    label = Label(window, text='Command completed.')
-    label.pack()
+    info_label = Label(window, text='Command completed.')
+    info_label.pack()
 
     window.config(cursor="")
 
@@ -93,11 +93,6 @@ def update_redcap_data():
 
 
 if __name__ == "__main__":
-
-
-# ----------------------------------------------------------------------------------------------------------------------
-#  UI Code
-# ----------------------------------------------------------------------------------------------------------------------
 
     # Initialize the tcl/tk interpreter and create the root window.
     window = Tk()
@@ -118,5 +113,3 @@ if __name__ == "__main__":
 
     # Display window.
     window.mainloop()
-
-    update_redcap_data()
