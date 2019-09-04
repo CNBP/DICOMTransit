23768e3ecdd9f773028771e2bd5faeeb2da79c61
Yang Ding <dyt811@gmail.com>
2019-09-04 17:07:31 -0400

    Add PyCap as a dependency instead.
    Unit test past.

--- Python/redcap/test_APICall.py
+++ Python/redcap/test_APICall.py
@@ -12,3 +12,13 @@ def test_Post():
 
     response = post(URL, data=payload)
     print(response)
+
+
+def test_PyCap():
+    from PyCap.redcap import project
+
+    # Two constants we'll use throughout
+    TOKEN = environment.REDCAP_TOKEN_CNN_ADMISSION
+    URL = "https://redcap.cnbp.ca/api/"
+    project_admission = project.Project(URL, TOKEN)
+    data = project_admission.export_records()
